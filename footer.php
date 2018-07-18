cda4aa603dd0dbe58182bbb3233d34b75520da1d
Ninayatun <mahmudanurinayatun@gmail.com>
2018-07-18 14:25:30 +0700

    konten

---
+++ footer.php
@@ -0,0 +1,8 @@
+      <footer class="site-footer">
+          <div class="text-center">
+              2018 - PUTRA PURNAMA INDONESIA
+              <a href="blank.html#" class="go-top">
+                  <i class="fa fa-angle-up"></i>
+              </a>
+          </div>
+      </footer>
---
+++ navigation.php
@@ -0,0 +1,60 @@
+<aside>
+          <div id="sidebar"  class="nav-collapse ">
+              <!-- sidebar menu start-->
+              <ul class="sidebar-menu" id="nav-accordion">
+                    
+                  <li class="mt">
+                      <a href="home.php">
+                          <i class="fa fa-dashboard"></i>
+                          <span>Dashboard</span>
+                      </a>
+                  </li>
+
+                  <li class="sub-menu">
+                      <a href="anggota.php" >
+                          <i class="fa fa-users"></i>
+                          <span>Anggota</span>
+                      </a>
+                  </li>
+
+                  <li class="sub-menu">
+                      <a href="petugas.php" >
+                          <i class="fa fa-user"></i>
+                          <span>Petugas</span>
+                      </a>
+                  </li>
+                  <li class="sub-menu">
+                      <a href="buku.php" >
+                          <i class="fa fa-book"></i>
+                          <span>Buku</span>
+                      </a>
+                  </li>
+                  <li class="sub-menu">
+                      <a href="kategori.php" >
+                          <i class="fa fa-tasks"></i>
+                          <span>Kategori</span>
+                      </a>
+                  </li>
+                  <li class="sub-menu">
+                      <a href="penulis.php" >
+                          <i class="fa fa-user"></i>
+                          <span>Penulis</span>
+                      </a>
+                  </li>
+                  <li class="sub-menu">
+                      <a href="penerbit.php" >
+                          <i class="fa fa-user"></i>
+                          <span>Penerbit</span>
+                      </a>
+                  </li>
+                  <li class="sub-menu">
+                      <a href="peminjaman.php" >
+                          <i class=" fa fa-list-alt"></i>
+                          <span>Peminjaman</span>
+                      </a>
+                  </li>
+
+              </ul>
+              <!-- sidebar menu end-->
+          </div>
+      </aside>
