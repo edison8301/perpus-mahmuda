201a605b3c144394be45164b4424d0a175af9d31
Ninayatun <mahmudanurinayatun@gmail.com>
2018-07-20 11:43:06 +0700

    login dengan session

--- header.php
+++ header.php
@@ -32,7 +32,7 @@
             <!--logo end-->
             <div class="top-menu">
               <ul class="nav pull-right top-menu">
-                    <li><a class="logout" href="login.html">Logout</a></li>
+                    <li><a class="logout" href="logout.php">Logout</a></li>
               </ul>
             </div>
 </header>
--- kategori.php
+++ kategori.php
@@ -1,4 +1,25 @@
-<?php include 'koneksi.php';?>
+<?php 
+session_start();
+if (empty($_SESSION['username'])){
+  header('location:index.php');  
+} else {
+  include "koneksi.php";
+?>
+<?php
+                    $timeout = 10; // Set timeout minutes
+                    $logout_redirect_url = "index.php"; // Set logout URL
+
+                    $timeout = $timeout * 60; // Converts minutes to seconds
+                    if (isset($_SESSION['start_time'])) {
+                        $elapsed_time = time() - $_SESSION['start_time'];
+                        if ($elapsed_time >= $timeout) {
+                            session_destroy();
+                            echo "<script>alert('Session Anda Telah Habis!'); window.location = '$logout_redirect_url'</script>";
+                        }
+                    }
+                    $_SESSION['start_time'] = time();
+                    ?>
+                    <?php } ?>
 <?php include 'header.php';?>
 <?php include 'navigation.php';?>
 <html>
